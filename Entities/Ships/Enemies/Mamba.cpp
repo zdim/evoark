c69be2023fb5cf9e9968013a31bd34f6b2d327ef
Fiercy <Fiercy@me.com>
2014-03-24 19:37:16 -0400

    1

--- Entities/Ships/Enemies/Mamba.cpp
+++ Entities/Ships/Enemies/Mamba.cpp
@@ -1,6 +1,6 @@
 //
 #include "Mamba.h"
-
+#include "../../../Message System/CreateProjectile.h"
 
 CMamba::CMamba()
 {
@@ -9,4 +9,49 @@ CMamba::CMamba()
 
 CMamba::~CMamba()
 {
+}
+
+
+void CMamba::Update(float dt)
+{
+	SGD::Vector dir = CCopperhead::AI(dt);
+	CEnemy::Update(dt);
+	CEntity::Update(dt);
+
+	m_fMissileTimer += dt;
+	//Determine rotation and dir, but NOT velocity
+
+	if (GetTarget() != nullptr)
+	{
+		SGD::Vector vToTarget =
+		{
+			GetTarget()->GetPosition().x - this->GetPosition().x,
+			GetTarget()->GetPosition().y - this->GetPosition().y,
+		};
+
+
+		if (vToTarget.ComputeLength() <= 800)
+		{
+			CreateMissile();
+		}
+
+	}
+}
+
+void CMamba::AddGrav(SGD::Vector grav)
+{
+	
+}
+
+
+
+void CMamba::CreateMissile()
+{
+	if (m_fMissileTimer <= m_fMissileDelay)
+		return;
+	m_fMissileTimer = 0;
+	//TODO: Send CreateMissile message
+
+	CreateProjectileMessage* msg = new CreateProjectileMessage(EntityType::Missile, position, size, rotation, damage);
+	msg->QueueMessage();
 }
--- Entities/Ships/Enemies/Mamba.h
+++ Entities/Ships/Enemies/Mamba.h
@@ -4,12 +4,22 @@
 class CMamba :
 	public CCopperhead
 {
+private:
+	float m_fMissileDelay;
+	float m_fMissileTimer;
+	int   m_nMissileDamage; 
+
 public:
 	CMamba();
 	virtual ~CMamba();
+	int GetType() override { return (int)EntityType::Mamba; }
+	void Update(float dt);
+	void AddGrav(SGD::Vector);
+	void CreateMissile();
 };
 
 class CMambaCoord : public CMamba, public Coordinator
 {
 
 };
+
