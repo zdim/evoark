1abee9e619f26c1d5df78b40302b9756b7d35994
Vladimir <Fiercy@me.com>
2014-03-26 22:30:53 -0400

    Testing warp

--- Entities/Ships/Enemies/Cobra.cpp
+++ Entities/Ships/Enemies/Cobra.cpp
@@ -4,6 +4,10 @@
 
 CCobra::CCobra()
 {
+	warpTimer = 0;
+	warpDelay = 4;
+	warpDuration = 1;
+	warpSpeed = 45;
 }
 
 
@@ -13,9 +17,26 @@ CCobra::~CCobra()
 
 void CCobra::Update(float dt)
 {
+	warpTimer += dt; 
 	SGD::Vector dir = CCopperhead::AI(dt);
+
+	Warp();
+
+	if (warpTimer <= warpDuration)
+	{
+		velocity = dir * (speed + warpSpeed);
+	}
+	else
+		velocity = dir * speed; 
+
 	CEnemy::Update(dt);
 	CEntity::Update(dt);
+}
+
+void CCobra::Warp()
+{
+	if (warpTimer > warpDelay)
+		warpTimer = 0;
 }
 
 void CCobra::AddGrav(SGD::Vector grav)
